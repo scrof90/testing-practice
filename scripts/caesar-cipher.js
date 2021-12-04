f2124773fb301146f838ee1eb8d7e84fdf565fe4
scrof90 <asknyshev@gmail.com>
2021-12-05 00:34:03 +0200

    refactor caesarCipher()

--- scripts/caesar-cipher.js
+++ scripts/caesar-cipher.js
@@ -1,47 +1,35 @@
 const caesarCipher = (string) => {
   // map cipher rules to ascii character codes
-  const cipher = {
-    offsets: {
-      normal: 1,
-      z: -25,
-      symbol: 0,
-    },
-
-    // character codes for edge cases
-    charCodes: {
-      upperA: 65,
-      upperZ: 90,
-      lowerA: 97,
-      lowerZ: 122,
-    },
+  const offsets = {
+    normal: 1,
+    z: -25,
+    symbol: 0,
   };
 
-  const encryptedString = string
-    .split('')
-    .map((char) => {
-      const charCode = char.charCodeAt(0);
+  // character codes for edge cases
+  const charCodes = {
+    upperA: 65,
+    upperZ: 90,
+    lowerA: 97,
+    lowerZ: 122,
+  };
 
-      // choose the correct character code offset according to case
-      let offset;
-      if (
-        charCode < cipher.charCodes.upperA ||
-        charCode > cipher.charCodes.lowerZ
-      ) {
-        offset = cipher.offsets.symbol;
-      } else if (
-        charCode === cipher.charCodes.lowerZ ||
-        charCode === cipher.charCodes.upperZ
-      ) {
-        offset = cipher.offsets.z;
-      } else {
-        offset = cipher.offsets.normal;
-      }
+  const encryptChar = (char) => {
+    const charCode = char.charCodeAt(0);
+    // choose the correct character code offset according to case
+    let offset;
+    if (charCode < charCodes.upperA || charCode > charCodes.lowerZ) {
+      offset = offsets.symbol;
+    } else if (charCode === charCodes.lowerZ || charCode === charCodes.upperZ) {
+      offset = offsets.z;
+    } else {
+      offset = offsets.normal;
+    }
+    const encryptedCharCode = charCode + offset;
+    return String.fromCharCode(encryptedCharCode);
+  };
 
-      // encrypt the character by applying the offset to its character code
-      const encryptedCharCode = charCode + offset;
-      return String.fromCharCode(encryptedCharCode);
-    })
-    .join('');
+  const encryptedString = string.split('').map(encryptChar).join('');
 
   return encryptedString;
 };
