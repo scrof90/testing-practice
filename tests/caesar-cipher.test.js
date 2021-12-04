009468ff118b4cae94049def15e8f978cab62d87
scrof90 <asknyshev@gmail.com>
2021-12-05 01:12:04 +0200

    finish tests for caesarCipher()

--- tests/caesar-cipher.test.js
+++ tests/caesar-cipher.test.js
@@ -1,21 +1,51 @@
 import caesarCipher from '../scripts/caesar-cipher';
 
 describe('caesarCipher()', () => {
+  test('empty string should return empty string', () => {
+    expect(caesarCipher('')).toBe('');
+  });
+
   describe('One character strings:', () => {
-    test('"a" should become "b"', () => {
+    test('"a" should return "b"', () => {
       expect(caesarCipher('a')).toBe('b');
     });
 
-    test('"B" should become "C"', () => {
+    test('"B" should return "C"', () => {
       expect(caesarCipher('B')).toBe('C');
     });
 
-    test('edge case: "z" should wrap to "a"', () => {
+    test('"z" should wrap to "a"', () => {
       expect(caesarCipher('z')).toBe('a');
     });
 
-    test('edge case: "Z" should wrap to "A"', () => {
+    test('"Z" should wrap to "A"', () => {
       expect(caesarCipher('Z')).toBe('A');
+    });
+  });
+
+  describe('One-word strings:', () => {
+    test('"word" should return "xpse"', () => {
+      expect(caesarCipher('word')).toBe('xpse');
+    });
+
+    test('"Zombie" should return "Apncjf"', () => {
+      expect(caesarCipher('Zombie')).toBe('Apncjf');
+    });
+  });
+
+  describe('Sentences:', () => {
+    it('should handle whole sentences', () => {
+      const sentence = 'The quick brown fox jumps over the lazy dog';
+      const encryptedSentence = 'Uif rvjdl cspxo gpy kvnqt pwfs uif mbaz eph';
+      expect(caesarCipher(sentence)).toBe(encryptedSentence);
+    });
+
+    it('should handle punctuation', () => {
+      const sentence =
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+      const encryptedSentence =
+        'Mpsfn jqtvn epmps tju bnfu, dpotfdufuvs bejqjtdjoh fmju, tfe ep fjvtnpe ufnqps jodjejevou vu mbcpsf fu epmpsf nbhob bmjrvb.';
+      expect(caesarCipher(sentence)).toBe(encryptedSentence);
     });
   });
 });
