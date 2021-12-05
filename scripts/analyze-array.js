5855daab2d57073e2719333220720eca57ee101c
scrof90 <asknyshev@gmail.com>
2021-12-05 02:46:57 +0200

    add scaffolding for analyzeArray() function

---
+++ scripts/analyze-array.js
@@ -0,0 +1,6 @@
+// An analyzeArray function that takes an array of numbers and returns
+// an object with the following properties: average, min, max, and length.
+
+const analyzeArray = (arr) => {};
+
+export default analyzeArray;
