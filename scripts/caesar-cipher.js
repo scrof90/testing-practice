e743a3c8f3e93ecd72077e560c387de124bd534f
scrof90 <asknyshev@gmail.com>
2021-12-05 02:51:40 +0200

    refactor caesarCipher()

--- scripts/caesar-cipher.js
+++ scripts/caesar-cipher.js
@@ -1,4 +1,4 @@
-const caesarCipher = (string) => {
+const encryptChar = (char) => {
   // map cipher rules to ascii character codes
   const offsets = {
     normal: 1,
@@ -14,24 +14,21 @@ const caesarCipher = (string) => {
     lowerZ: 122,
   };
 
-  const encryptChar = (char) => {
-    const charCode = char.charCodeAt(0);
-    // choose the correct character code offset according to case
-    let offset;
-    if (charCode < charCodes.upperA || charCode > charCodes.lowerZ) {
-      offset = offsets.symbol;
-    } else if (charCode === charCodes.lowerZ || charCode === charCodes.upperZ) {
-      offset = offsets.z;
-    } else {
-      offset = offsets.normal;
-    }
-    const encryptedCharCode = charCode + offset;
-    return String.fromCharCode(encryptedCharCode);
-  };
+  const charCode = char.charCodeAt(0);
+  // choose the correct character code offset according to case
+  let offset;
+  if (charCode < charCodes.upperA || charCode > charCodes.lowerZ) {
+    offset = offsets.symbol;
+  } else if (charCode === charCodes.lowerZ || charCode === charCodes.upperZ) {
+    offset = offsets.z;
+  } else {
+    offset = offsets.normal;
+  }
 
-  const encryptedString = string.split('').map(encryptChar).join('');
-
-  return encryptedString;
+  const encryptedCharCode = charCode + offset;
+  return String.fromCharCode(encryptedCharCode);
 };
+
+const caesarCipher = (string) => string.split('').map(encryptChar).join('');
 
 export default caesarCipher;
